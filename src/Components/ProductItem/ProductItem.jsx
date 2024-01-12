e9284cc2b9567e94b405aae6332c6c8c3e18c220
Bohdan Shyiatyi <koder2563@gmail.com>
2024-01-12 20:38:09 +0100

    fx

--- src/Components/ProductItem/ProductItem.jsx
+++ src/Components/ProductItem/ProductItem.jsx
@@ -19,7 +19,7 @@ const ProductItem = ({ product, className, onAdd, onDelete, backet }) => {
 
     return (
         <div className={'product ' + className}>
-            {getCount() < 1 && <div className={'counter'}>{getCount()}</div>}
+            <div className={'counter'}>{getCount()}</div>
             <img src={product.image} className={'img'}/>
             <div className={'title'}>{product.name}</div>
             <div className={'description'}>{product.description}</div>
