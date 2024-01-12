9e3c633a9852fb998ca83ac3eb3bc246a9937b45
Bohdan Shyiatyi <koder2563@gmail.com>
2024-01-12 19:05:55 +0100

    test

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Button from "../Button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
 import './Header.css';
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
-    const {user, onClose} = useTelegram();
+    const { user, onClose } = useTelegram();
+    
+    const navigate = useNavigate();
 
     return (
         <div className={'header'}>
+            <Button className='btn-minus' onClick={() => navigate('/')}>Меню</Button>
             <Button onClick={onClose}>Создать пиццу</Button>
         </div>
     )
